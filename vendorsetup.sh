88ed396e26610374d179b744db02bb63c4e821e2
DevAdalat <akbusiness022@gmail.com>
2023-07-15 09:13:37 +0530

    gauguin: Added vendorsetup.sh and use cosmic clang to build kernel

---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+echo 'Starting to clone stuffs needed for your device'
+
+echo 'Cloning Vendor tree [1/3]'
+# Vendor Tree
+git clone --depth=1 https://github.com/Niyush-04/vendor_xiaomi_gauguin.git vendor/xiaomi/gauguin
+
+echo 'Cloning Kernel tree [2/3]'
+# Kernel Tree
+git clone --depth=1 https://github.com/DevAdalat/kernel_xiaomi_gauguin.git --recurse-submodules kernel/xiaomi/gauguin
+
+echo 'Cloning atomx clang [3/3]'
+# atomx Clang
+git clone --depth=1 https://gitlab.com/GhostMaster69-dev/cosmic-clang.git prebuilts/clang/kernel/linux-x86/clang-cosmic
+
+echo 'Completed, Now proceeding to lunch'
